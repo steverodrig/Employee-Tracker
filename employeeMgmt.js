8ecff13b58c70c601c920468f0ff26f7240f0ca7
Steven Rodriguez <srrodrig.rodriguez@gmail.com>
2020-10-13 13:27:15 -0500

    Added initial prompt and view all employee path

--- employeeMgmt.js
+++ employeeMgmt.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var ctable = require("console.table");
-const { start } = require("repl");
+//const { start } = require("repl");
+//const { exit } = require("process");
 
 
-var connection = mysql.connection({
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -12,8 +13,81 @@ var connection = mysql.connection({
     database: "employee_db"
 });
 
-connection.connect(function(err) {
-    if(err) throw(err);
+connection.connect(function (err) {
+    if (err) throw (err);
     console.log("connected as id " + connection.threadId);
     start();
 });
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View employees", "Edit departments", "Edit roles", "Edit managers", "Exit"]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View employees":
+                    viewEmps();
+                    break;
+
+                case "Edit departments":
+                    editDepts();
+                    break;
+
+                case "Edit roles":
+                    editRoles();
+                    break;
+
+                case "Edit managers":
+                    editMgmt();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+};
+function viewEmps() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View all employees", "View employees by department", "View employees by manager", "Go back", "Exit"]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View all employees":
+                    viewAll();
+                    break;
+
+                case "View employees by department":
+                    viewDepts();
+                    break;
+
+                case "View employees by manager":
+                    viewMgmt();
+                    break;
+
+                case "Go back":
+                    start();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+};
+
+function viewAll() {
+    var query = "SELECT * FROM employees";
+    connection.query(query, function(err, res) {
+        if(err) throw(err);
+        console.table(res);
+    })
+}
--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -28,19 +28,22 @@ CREATE TABLE employees (
     manager_id INT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id)
-        REFERENCES role(id)
+        REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+		REFERENCES role(id)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 INSERT INTO department (name)
 VALUES ("Pathology"), ("Histology"), ("Office"), ("Maintenance");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Histology Manager", 85000, 1), ("Office Manager", 105000, 2), ("pathologist", 279000, 0), ("Histotech", 56000, 1), ("Grosser", 64000, 1), 
-("Transcriptionist", 42000, 2), ("Office Clerk", 40000, 2), ("Janitor", 24000, 3);
-
+VALUES ("Histology Manager", 85000, 2), ("Office Manager", 105000, 3), ("pathologist", 279000, 1), ("Histotech", 56000, 2), ("Grosser", 64000, 2), 
+("Transcriptionist", 42000, 3), ("Office Clerk", 40000, 3), ("Janitor", 24000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Joe", "Schmeaux", 1, 1), ("Sarah", "Plainentall", 2, 2), ("Bo", "Jangles", 3, 3), ("Everett", "Montain", 4, 4);
+VALUES ("Joe", "Schmeaux", 1, 1), ("Charles", "Stevens", 3, 2), ("Maren", "Chan", 3, 2), ("Heather", "Miller", 3, 2), ("Janie", "Lewis", 2, 2), ("Bo", "Jangles", 4, 1), 
+("Everett", "Montain", 4, 1), ("Tom", "Moffett", 4, 1), ("Nadine", "Peretta", 4, 1), ("Alicia", "Deleon", 3, 2), ("Diana", "Moffett", 3, 2), ("Rose", "Mcgowan", 3, 2), ("Steve", "Rodriguez", 3, 2);
 
 SELECT * FROM department;
 SELECT * FROM  role;
